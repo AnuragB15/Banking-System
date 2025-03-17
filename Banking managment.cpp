3ecd9172ed3abf575602e04d87fb04e7eb1f820e
Anurag Banerjee <142042238+AnuragB15@users.noreply.github.com>
2025-03-17 08:05:33 +0530

    Update Banking managment.cpp

--- Banking managment.cpp
+++ Banking managment.cpp
@@ -1,354 +1,142 @@
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-class Bank
-{
-    private:
-    
-        string id = "", name="", address="", accountType="", city="", state="";
-        
-        char type;
-        
-        int date = 0, month = 0, year = 0, pincode = 0, aadhar = 0, pan = 0;
-        
-        float balance = 0;
-    
-    public:
-    
-        string generateRandomID();
-    
-        void openAccount();
-        
-        void depositMoney();
-        
-        void withdrawMoney();
-        
-        void accountDetail();
+// Base class representing a generic bank account
+class Account {
+protected:
+    string id, name, address, city, state, accountType;
+    int date, month, year, pincode, aadhar, pan;
+    float balance;
+
+public:
+    Account() : balance(0), date(0), month(0), year(0), pincode(0), aadhar(0), pan(0) {}
+
+    // Encapsulation: Getters and setters
+    string getID() { return id; }
+    float getBalance() { return balance; }
+    string getAccountType() { return accountType; }
+
+    void setBalance(float amount) { balance = amount; }
+
+    virtual void openAccount();   // Abstraction
+    virtual void depositMoney();
+    virtual void withdrawMoney();
+    virtual void accountDetail();
+
+    string generateRandomID() {
+        random_device rd;
+        mt19937 rng(rd());
+        uniform_int_distribution<int> dist(0, 99999999);
+        ostringstream oss;
+        oss << setw(8) << setfill('0') << dist(rng);
+        return "NB" + to_string(year) + "-" + oss.str();
+    }
 };
 
-string Bank :: generateRandomID() 
-{
-    random_device rd;
-    
-    mt19937 rng(rd());
-    
-    uniform_int_distribution<int> dist(0, 99999999);
-
-    ostringstream oss; 
-    
-    oss << std::setw(8) << std::setfill('0') << dist(rng);
-    
-    return oss.str();
-}
-
-void Bank ::  openAccount()
-{
-    
-    system("clear");
-    
-    string text = "Open Account";
-    
-    string underline(text.length(), '-');
-    
-    cout<<text<<endl<<underline<<endl<<endl;
-    
-    cout<<"Enter your full name: ";
-    
-    cin.ignore();
-    
-    getline(cin>>ws, name);
-    
-    cout<<"Enter your city: ";
-    
-    getline(cin>>ws, city);
-    
-    cout<<"Enter your state: ";
-    
-    getline(cin>>ws, state);
-    
-    cout<<"Enter your address: ";
-    
-    getline(cin>>ws, address);
-    
-    cout<<"Enter your pincode: ";
-    
-    cin>>pincode;
-    
-    cout<<"Enter your birth year: ";
-    
-    cin>>year;
-    
-    cout<<"Enter your birth month(month number): ";
-    
-    cin>>month;
-    
-    cout<<"Enter your birth date: ";
-    
-    cin>>date;
-    
-    cout<<"Enter Aadhar number: ";
-    
-    cin>>aadhar;
-    
-    cout<<"Enter Pancard number: ";
-    
-    cin>>pan;
-    
-    string temp = generateRandomID();
-    
-    id = "NB" + to_string(year) + "-" + temp;
-    
-    system("clear");
-    
-    cout<<"To create current account click 'C'. To create savings account click 'S': ";
-    
-    cin>>type;
-    
-    system("clear");
-    
-    while(!(type == 'C' || type == 'c' || type == 'S' || type == 's' ))
-    {
+// Derived class for Savings Account
+class SavingsAccount : public Account {
+public:
+    void openAccount() override {
         system("clear");
-        
-        cout<<"Wrong key entered!! Re-enter the correct key to specify account type" << endl << endl;
-        
-        cout<<"To create current account click 'C'. To create savings account click 'S': ";
-        
-        cin>>type;
-    }
-    
-    system("clear");
-    
-    if(type == 'C' || type == 'c') accountType = "Current Account";
-    
-    else accountType = "Savings Account";
-    
-    cout<<"Please enter the minimum amount to complete the account opening process(Minimum ₹2000): ₹";
-    
-    cin>>balance;
-    
-    system("clear");
-    
-    while(balance < 2000)
-    {
-        system("clear");
-        
-        cout<<"Amount entered is not sufficient! Amount should be more than ₹2000." << endl;
-        
-        cout << "Enter amount: ₹";
-        
-        cin>>balance;
-    }
-    
-    system("clear");
-    
-    cout<<"Dear customer, your "<<accountType<<" have been created. Welcome to National Bank!";
-}
-
-void Bank :: depositMoney()
-{
-    
-    system("clear");
-    
-    string text = "Deposit Amount";
-    
-    string underline(text.length(), '-');
-    
-    cout<<text<<endl<<underline<<endl<<endl;
-    
-    int deposit;
-    
-    cout<<"Please enter the amount you want to deposit: ₹";
-    
-    cin>>deposit;
-    
-    balance = balance + deposit;
-    
-    cout<<"Dear customer, your current balance is: ₹"<<balance;
-    
-    cout<<endl;
-}
-
-void Bank :: withdrawMoney()
-{
-    
-    system("clear");
-    
-    string text = "Withdraw Amount";
-    
-    string underline(text.length(), '-');
-    
-    cout<<text<<endl<<underline<<endl<<endl;
-    
-    int withdraw;
-    
-    cout<<"Please enter amount you want to withdraw: ₹";
-    
-    cin>>withdraw;
-    
-    while(withdraw > balance)
-    {
-        cout<<"Insufficient balance!!"<<endl;
-        
-        cout<<"If you want to continue press 'Y', else if you want to exit press 'N': "<<endl;
-        
-        char temp;
-        
-        cin>>temp;
-        
-        if(temp == 'N' || temp == 'n') exit(0);
-        
-        else
-        {
-            cin>>withdraw;
+        cout << "Opening Savings Account...\n";
+        accountType = "Savings Account";
+        cout << "Enter your name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Enter initial deposit (Min ₹2000): ₹";
+        cin >> balance;
+        while (balance < 2000) {
+            cout << "Minimum balance should be ₹2000. Enter again: ₹";
+            cin >> balance;
         }
+        id = generateRandomID();
+        cout << "Savings Account created successfully. ID: " << id << endl;
     }
-    
-    balance = balance - withdraw;
-    
-    cout<<"Dear customer, your current balance is: ₹"<<balance;
-    
-    cout<<endl;
+};
 
-}
-
-void Bank :: accountDetail()
-{
-    
-    system("clear");
-    
-    string text = "Account Details:";
-    
-    string underline(text.length(), '-');
-    
-    cout<<text<<endl<<underline<<endl<<endl;
-    
-    cout<<"Customer-ID: "<<id<<endl;
-    
-    cout<<"Name: "<<name<<endl;
-    
-    string fullAddress = address + ", " + city + ", " + state;
-    
-    cout<<"Address: "<<fullAddress<<endl;
-    
-    cout<<"PINCODE: "<<pincode<<endl;
-    
-    cout<<"Date of Birth: "<<date<<"/"<<month<<"/"<<year<<endl;
-    
-    cout<<"Your current balance is: ₹"<<balance<<endl;
-    
-    cout<<"Your Aaadhar number is: "<<aadhar<<endl;
-    
-    cout<<"Your Pan card number is: "<<pan<<endl;
-}
-
-int main()
-{
-    
-    int option, continueProcess;
-    
-    Bank* obj = new Bank();
-    
-    string header = "Welcome to National Bank!";
-    
-    string underline1(header.length(), '-');
-    
-    cout<<header<<endl<<underline1<<endl<<endl;
-    
-    bool accountStatus = false;
-    
-    cout<<"To proceed accordingly please select any one of the options given below: "<<endl<<endl;
-    
-    do{
-        
-        cout<<"1) Open Account."<<endl;
-    
-        cout<<"2) Deposit Money."<<endl;
-    
-        cout<<"3) Withdraw Money."<<endl;
-    
-        cout<<"4) Display Account Detail."<<endl;
-    
-        cout<<"5) Exit."<<endl<<endl;
-        
-        cin>> option;
-        
+// Derived class for Current Account
+class CurrentAccount : public Account {
+public:
+    void openAccount() override {
         system("clear");
+        cout << "Opening Current Account...\n";
+        accountType = "Current Account";
+        cout << "Enter your name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Enter initial deposit (Min ₹5000): ₹";
+        cin >> balance;
+        while (balance < 5000) {
+            cout << "Minimum balance should be ₹5000. Enter again: ₹";
+            cin >> balance;
+        }
+        id = generateRandomID();
+        cout << "Current Account created successfully. ID: " << id << endl;
+    }
+};
+
+// Polymorphism through pointers
+int main() {
+    Account* account = nullptr;
+    int choice;
+    bool accountCreated = false;
+
+    do {
+        system("clear");
+        cout << "1) Open Account\n2) Deposit Money\n3) Withdraw Money\n4) View Account Details\n5) Exit\n";
+        cin >> choice;
         
-        switch(option)
-        {
+        switch (choice) {
             case 1:
-            
-            obj->openAccount();
-            
-            accountStatus = true;
-            
-            break;
-            
+                cout << "Select Account Type (1: Savings, 2: Current): ";
+                int type;
+                cin >> type;
+                if (type == 1)
+                    account = new SavingsAccount();
+                else
+                    account = new CurrentAccount();
+                
+                account->openAccount();
+                accountCreated = true;
+                break;
+
             case 2:
-            
-            if(accountStatus) obj->depositMoney();
-            
-            else
-            {
-                cout<<"No account found!"<<endl; 
-            
-                cout<<"Create an account.";
-            }
-            
-            break;
-            
+                if (accountCreated) {
+                    account->depositMoney();
+                } else {
+                    cout << "No account found! Please open an account first.\n";
+                }
+                break;
+
             case 3:
-            
-            if(accountStatus) obj->withdrawMoney();
-            
-            else
-            {
-                cout<<"No account found!"<<endl;
-            
-                cout<<"Create an account.";
-            }
-            
-            break;
-            
+                if (accountCreated) {
+                    account->withdrawMoney();
+                } else {
+                    cout << "No account found! Please open an account first.\n";
+                }
+                break;
+
             case 4:
-            
-            if(accountStatus) obj->accountDetail();
-            
-            else
-            {
-                cout<<"No account found!"<<endl;
-            
-                cout<<"Create an account.";
-            }
-            
-            break;
-            
+                if (accountCreated) {
+                    account->accountDetail();
+                } else {
+                    cout << "No account found! Please open an account first.\n";
+                }
+                break;
+
             case 5:
-            
-            exit(0);
-            
+                cout << "Exiting...\n";
+                delete account;
+                exit(0);
+
             default:
-            
-            cout<<"Wrong key entered! Try again."<<endl;
-            
-            break;
+                cout << "Invalid choice! Try again.\n";
         }
-        
-        cout<<endl;
-        
-        cout<<"If you want to continue press 1, else if you want to exit press 0"<<endl;
-        
-        system("clear");
-        
-        cin>>continueProcess;
-        
-        if(!continueProcess) exit(0);
-    }
-    
-    while(continueProcess);
+        cout << "Press Enter to continue...\n";
+        cin.ignore();
+        cin.get();
+    } while (true);
 
     return 0;
 }
